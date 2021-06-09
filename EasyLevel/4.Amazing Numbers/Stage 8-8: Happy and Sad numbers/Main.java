bc50a9627ca47327a51b1628b54ad99535118aa9
Apichai gusaljeerung <71862279+superjacky-apichai@users.noreply.github.com>
2021-06-09 22:30:01 +0700

    Add files via upload

---
+++ EasyLevel/4.Amazing Numbers/Stage 8-8: Happy and Sad numbers/Main.java
@@ -0,0 +1,78 @@
+package numbers;
+
+
+import java.util.Scanner;
+//Stage 8/8: Happy and Sad
+public class Main {
+    final private static Scanner scanner = new Scanner(System.in);
+    final private static String welcomeText = "Welcome to Amazing Numbers!\n" +
+            "\n" +
+            "Supported requests:\n" +
+            "- enter a natural number to know its properties;\n" +
+            "- enter two natural numbers to obtain the properties of the list:\n" +
+            "  * the first parameter represents a starting number;\n" +
+            "  * the second parameter shows how many consecutive numbers are to be printed;\n" +
+            "- two natural numbers and properties to search for;\n" +
+            "- a property preceded by minus must not be present in numbers;\n" +
+            "- separate the parameters with one space;\n" +
+            "- enter 0 to exit";
+
+
+    public static void main(String[] args) {
+        System.out.println(welcomeText);
+
+        while (true) {
+            System.out.print("Enter a request: ");
+
+            String[] forCountElement = scanner.nextLine().split("\\s");
+
+            try {
+
+                long naturalNumber = Long.parseLong(forCountElement[0]);
+                if (naturalNumber < 0) {
+                    System.out.println("The first parameter should be a natural number or zero.");
+                    continue;
+                }
+
+            } catch (Exception e) {
+                System.out.println("The first parameter should be a natural number or zero.");
+                continue;
+            }
+
+
+            long naturalNumber = Long.parseLong(forCountElement[0]);
+
+
+            if (naturalNumber == 0) {
+                System.out.println("Goodbye!");
+                break;
+            } else if (forCountElement.length == 1) {
+                LessThanTwoElement.oneElement(naturalNumber);
+            } else {
+                try {
+                    long secondElement = Long.parseLong(forCountElement[1]);
+                    if (secondElement < 1) {
+                        System.out.println("The second parameter should be a natural number.");
+                        continue;
+                    }
+
+
+                } catch (Exception e) {
+                    System.out.println("The second parameter should be a natural number.");
+                    continue;
+                }
+
+                if (forCountElement.length == 2) {
+                    LessThanTwoElement.twoElement(forCountElement);
+                } else {
+                    FindNumberMoreThanThreeElement.moreThanThreeElement(forCountElement);
+                }
+
+            }
+
+        }
+    }
+
+
+}
+
