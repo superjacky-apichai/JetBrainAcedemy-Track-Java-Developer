f08d2274425dd4f59ea65077bc410c69362b2249
Apichai gusaljeerung <71862279+superjacky-apichai@users.noreply.github.com>
2021-10-04 18:26:36 +0700

    Add files via upload

---
+++ 1.EASY/1.Simple Tic-Tac-Toe/Stage 1-5: Welcome to the battlefield/Stage 1-5 Welcome to the battlefield.java
@@ -0,0 +1,13 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("X O X\n" +
+                "O X O\n" +
+                "X X O ");
+
+
+        int a = -1;
+        System.out.println(1 - a++);
+    }
+}
