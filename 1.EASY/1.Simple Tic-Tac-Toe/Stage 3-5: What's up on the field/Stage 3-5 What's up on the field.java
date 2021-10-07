cadbc99e514fa10265d9753972731f206a69ac1a
Apichai gusaljeerung <71862279+superjacky-apichai@users.noreply.github.com>
2021-10-07 19:39:12 +0700

    Update Stage 3-5 What's up on the field.java

--- 1.EASY/1.Simple Tic-Tac-Toe/Stage 3-5: What's up on the field/Stage 3-5 What's up on the field.java
+++ 1.EASY/1.Simple Tic-Tac-Toe/Stage 3-5: What's up on the field/Stage 3-5 What's up on the field.java
@@ -1,12 +1,11 @@
 package tictactoe;
 
-import java.util.Arrays;
+
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
         String[][] xoField = new String[3][3];
 
