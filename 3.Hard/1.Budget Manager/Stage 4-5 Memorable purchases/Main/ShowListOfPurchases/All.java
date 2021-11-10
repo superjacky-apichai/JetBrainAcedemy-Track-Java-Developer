9f5773d2a12d69558db34e651b7f1c937c98d977
Apichai gusaljeerung <71862279+superjacky-apichai@users.noreply.github.com>
2021-11-10 19:06:51 +0700

    Add files via upload

---
+++ 3.Hard/1.Budget Manager/Stage 4-5 Memorable purchases/Main/ShowListOfPurchases/All.java
@@ -0,0 +1,50 @@
+package budget.ShowListOfPurchases;
+
+
+import budget.Menu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class All implements Menu {
+
+    Map<String, Double> purchase = new HashMap<>();
+
+    public void getPurchase() {
+        if (purchase.size() == 0) {
+            System.out.println("The purchase list is empty!");
+        } else {
+            System.out.println("ALL:");
+            double sum = 0;
+            for (var i : purchase.entrySet()) {
+                System.out.printf("%s $%.2f\n", i.getKey(), i.getValue());
+                sum += i.getValue();
+            }
+            System.out.printf("Total sum: $%.2f\n", sum);
+        }
+    }
+
+    public void setPurchase(String k, Double v) {
+        this.purchase.put(k, v);
+    }
+
+    public int getSize() {
+        return this.purchase.size();
+    }
+
+    public Map getMap() {
+        return this.purchase;
+    }
+
+    @Override
+    public void getLoad(String[] fileLoad) {
+
+        for (int i = 0; i < fileLoad.length; i++) {
+            fileLoad[i] = fileLoad[i].trim();
+            String[] forSplit = fileLoad[i].split("=");
+            this.purchase.put(forSplit[0], Double.valueOf(forSplit[1]));
+        }
+    }
+
+
+}
---
+++ 3.Hard/1.Budget Manager/Stage 4-5 Memorable purchases/Main/ShowListOfPurchases/Other.java
@@ -0,0 +1,48 @@
+package budget.ShowListOfPurchases;
+
+import budget.Menu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Other implements Menu {
+    Map<String, Double> purchase = new HashMap<>();
+
+
+    public void getPurchase() {
+        if (purchase.size() == 0) {
+            System.out.println("The purchase list is empty!");
+        } else {
+            System.out.println("Other:");
+            double sum = 0;
+            for (var i : purchase.entrySet()) {
+                System.out.printf("%s $%.2f\n", i.getKey(), i.getValue());
+                sum += i.getValue();
+            }
+            System.out.printf("Total sum: $%.2f\n", sum);
+        }
+    }
+
+
+    public void setPurchase(String k, Double v) {
+        this.purchase.put(k, v);
+    }
+
+    public int getSize() {
+        return this.purchase.size();
+
+    }
+
+    public Map getMap() {
+        return this.purchase;
+    }
+
+    public void getLoad(String[] fileLoad) {
+
+        for (int i = 0; i < fileLoad.length; i++) {
+            fileLoad[i] = fileLoad[i].trim();
+            String[] forSplit = fileLoad[i].split("=");
+            this.purchase.put(forSplit[0], Double.valueOf(forSplit[1]));
+        }
+    }
+}
